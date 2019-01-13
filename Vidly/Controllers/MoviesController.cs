a72167caa0a4d95ababbb9ede4a9ed5c512b02b6
unknown <yousef.dev.c@gmail.com>
2019-01-13 11:12:51 +0200

    Section 3 Exercise 3

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -5,16 +5,40 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Movies
         public ActionResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
             return View(movies);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
         }
 
         public ActionResult Random()
@@ -25,8 +49,8 @@ namespace Vidly.Controllers
                 new Customer { Name = "Customer 1" },
                 new Customer { Name = "Customer 2" }
             };
-            var viewModel = new RandomMovieViewModel 
-            { 
+            var viewModel = new RandomMovieViewModel
+            {
                 Movie = movie,
                 Customers = customers
             };
@@ -39,13 +63,5 @@ namespace Vidly.Controllers
             return Content(year + "/" + month);
         }
 
-        private List<Movie> GetMovies() 
-        {
-            return new List<Movie> 
-            {
-                new Movie { Id = 1, Name = "Die Hard" },
-                new Movie { Id = 2, Name = "Bird Box" }
-            };
-        }
     }
 }
